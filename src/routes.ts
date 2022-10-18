656f4d0c33d4720252e3059c100b3662b08b6799
Thiago Vieira <tmaciel@digital-works.com>
2022-10-18 08:21:21 +0100

    Adicionando os tipos

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response) {
+    
+    CreateCourseService.execute({
+        name: "NodeJS", 
+        duration: 10, 
+        educator: "Thiago"
+    });
+
+    return response.send();
+
+}
